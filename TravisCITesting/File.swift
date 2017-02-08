356c04d5b25d5f07c69c1e339959e2d8f6038c4c
Joe Riess <joseph.riess@Joes-MBP.home>
2017-02-07 21:39:27 -0500

    travis yml, test unit test added

---
+++ TravisCITesting/File.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  TravisCITesting
+//
+//  Created by Joe Riess on 2/7/17.
+//  Copyright © 2017 Joe Riess. All rights reserved.
+//
+
+import Foundation
+
+struct Test {
+    
+    static func add(_ number: Int, to anotherNumber: Int) -> Int {
+        return number + anotherNumber
+    }
+}
